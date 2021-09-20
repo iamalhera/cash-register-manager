c75c04b7870ec63be2c6b74b8b9016f9ed32e5d2
Al Hera Ahmad <alheraahmad0786@gmail.com>
2021-09-21 00:05:21 +0530

    done 30%

---
+++ app.js
@@ -0,0 +1,12 @@
+var billAmount = document.querySelector("#textarea1");
+var cashGiven = document.querySelector("#textarea2");
+var checkBtn = document.querySelector("#check-button");
+var result = document.querySelector("#textarea3");
+
+
+function clickEventHandler(){
+    console.log("click ho gaya bhai");
+}
+
+checkBtn.addEventListener("click",clickEventHandler) ;
+
