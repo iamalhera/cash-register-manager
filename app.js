d999d6b9912eb0591055caf932e30bfb80c67b55
Al Hera Ahmad <alheraahmad0786@gmail.com>
2021-09-22 00:41:50 +0530

    feat : processing of notes

--- app.js
+++ app.js
@@ -1,19 +1,37 @@
 const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-given");
 const checkBtn = document.querySelector("#check-button");
-const message = document.querySelector("#error-message") ;
+const message = document.querySelector("#error-message");
+const noOfNotes = document.querySelectorAll(".no-of-notes");
+const returnedAmount = document.querySelector("#returned-amount") ;
 
-
-function kaamkarjabhai(){
-    message.style.display = "none" ;
-    if (billAmount.value < 0){
-        message.style.display = "block" ;
-       message.innerText = "aisa kaisa chalega" ;
-    } else{}
-    if (cashGiven.value < billAmount.value){
-        message.style.display = "block" ;
-        message.innerText = "fir se aisa kaisa chalega didi" ;
+const setOfNotes = [2000,500,100,50,20,5,1] ;
+function validateBillAndCashAmount() {
+    message.style.display = "none";
+    if (billAmount.value > 0) {
+        if (cashGiven.value >= billAmount.value) {
+            const amountToBeReturned = cashGiven.value - billAmount.value;
+            returnedAmount.innerText = amountToBeReturned ;
+            calculateChange(amountToBeReturned) ;
+        }
+        else {
+            message.style.display = "block";
+            message.innerText = "cash should be equal or more for transactions";
+        }
+    } else {
+        message.style.display = "block";
+        message.innerText = "invalid bill amount : Should be greated than 0";
     }
+
 }
 
-checkBtn.addEventListener("click", kaamkarjabhai) ; 
+function calculateChange(retunAmount){
+        for(let i= 0 ; i<setOfNotes.length ; i++){
+            const calculatedNumberOfNotes = Math.trunc(retunAmount/setOfNotes[i]) ;
+            retunAmount = retunAmount % setOfNotes[i] ;
+            noOfNotes[i].innerText = calculatedNumberOfNotes ;
+        } 
+        
+}
+
+checkBtn.addEventListener("click", validateBillAndCashAmount);
