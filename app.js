92ae5249ceb8477754651fa19e855983a6e2e47a
Al Hera Ahmad <alheraahmad0786@gmail.com>
2021-09-23 08:43:21 +0530

    fixed bugs of index, css and js file

--- app.js
+++ app.js
@@ -1,37 +1,73 @@
 const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-given");
-const checkBtn = document.querySelector("#check-button");
-const message = document.querySelector("#error-message");
+const checkButton = document.querySelector("#check-button");
+const errorMessage = document.querySelector("#error-message");
+const returningAmount = document.querySelector("#returning-amount");
 const noOfNotes = document.querySelectorAll(".no-of-notes");
-const returnedAmount = document.querySelector("#returned-amount") ;
 
-const setOfNotes = [2000,500,100,50,20,5,1] ;
-function validateBillAndCashAmount() {
-    message.style.display = "none";
+const outputTable = document.querySelector("#output-table");
+const returnChange = document.querySelector("#return-change");
+const nextButton = document.querySelector("#next-button") ;
+const divCashGiven = document.querySelector("#div-cash-given");
+
+const setOfNotes= [2000,500,100,50,10,5,1] ;
+
+divCashGiven.style.display = "none";
+outputTable.style.display = "none";
+returnChange.style.display = "none";
+checkButton.style.display = "none";
+
+function nextButtonHandler(){
+    hideErrorMessage() ;
     if (billAmount.value > 0) {
-        if (cashGiven.value >= billAmount.value) {
-            const amountToBeReturned = cashGiven.value - billAmount.value;
-            returnedAmount.innerText = amountToBeReturned ;
-            calculateChange(amountToBeReturned) ;
-        }
-        else {
-            message.style.display = "block";
-            message.innerText = "cash should be equal or more for transactions";
-        }
+        nextButton.style.display = "none";
+        divCashGiven.style.display = "block";
+        checkButton.style.display = "inline";
     } else {
-        message.style.display = "block";
-        message.innerText = "invalid bill amount : Should be greated than 0";
+        errorMessagefunc("Enter a valid amount");
     }
-
 }
 
-function calculateChange(retunAmount){
-        for(let i= 0 ; i<setOfNotes.length ; i++){
-            const calculatedNumberOfNotes = Math.trunc(retunAmount/setOfNotes[i]) ;
-            retunAmount = retunAmount % setOfNotes[i] ;
-            noOfNotes[i].innerText = calculatedNumberOfNotes ;
-        } 
-        
+function checkButtonHandler(){
+    hideErrorMessage() ;
+    
+    let billAmountValue = Number(billAmount.value);
+    let cashGivenValue = Number(cashGiven.value);
+    if (billAmountValue<0){
+        invalidErrorHandler("Enter valid bill amount") ;
+    }
+    else {
+        if(billAmountValue > cashGivenValue ){
+        errorMessagefunc("Give More Cash") ;
+        }
+        else {const returnAmount= cashGivenValue - billAmountValue ;
+        calculateChange(returnAmount) ;
+         }
+}
 }
 
-checkBtn.addEventListener("click", validateBillAndCashAmount);
+function invalidErrorHandler(msg){
+    errorMessage.innerText = msg ;
+}
+function hideErrorMessage(){
+    errorMessage.style.display = "none" ;
+}
+function errorMessagefunc(message){
+    outputTable.style.display = "none";
+    returnChange.style.display = "none";
+    errorMessage.style.display = "block" ;
+    errorMessage.innerText = message ;
+}
+function calculateChange(amountToBereturned){
+    outputTable.style.display = "inline-block" ;
+    returnChange.style.display = "inline-block" ;
+    returningAmount.innerText = amountToBereturned ;
+    for(let i=0 ; i<setOfNotes.length ; i++){
+        const calculatedNoOfNotes = Math.trunc(amountToBereturned / setOfNotes[i]) ;
+        amountToBereturned = amountToBereturned % setOfNotes[i] ;
+        noOfNotes[i].innerText = calculatedNoOfNotes ;
+    }
+}
+
+nextButton.addEventListener("click",nextButtonHandler);
+checkButton.addEventListener("click",checkButtonHandler) ;
